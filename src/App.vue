c21de49725ee148f9f9c88c41ff7181c5f307f3e
‘nicolaszorth’ <nicolaszorth@outlook.com>
2024-03-22 15:18:34 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <a-layout style="min-height: 100vh">
+    <a-layout-sider v-model:collapsed="collapsed" collapsible>
+      <SideBarLayout></SideBarLayout>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0" />
+      <a-layout-content style="margin: 0 16px">
+        <ContentLayout />
+      </a-layout-content>
+      <a-layout-footer style="text-align: center">
+        nicolaszorth ©2024 Created by zorth
+      </a-layout-footer>
+    </a-layout>
+  </a-layout>
+</template>
+<script lang="ts" setup>
+import SideBarLayout from '@/components/layout/SideBarLayout.vue';
+import ContentLayout from '@/components/layout/ContentLayout.vue'
+import { ref } from 'vue';
+const collapsed = ref<boolean>(false);
+</script>
+<style scoped>
+#components-layout-demo-side .logo {
+  height: 32px;
+  margin: 16px;
+  background: rgba(255, 255, 255, 0.3);
+}
+
+.site-layout .site-layout-background {
+  background: #fff;
+}
+
+[data-theme='dark'] .site-layout .site-layout-background {
+  background: #141414;
+}
+</style>
---
+++ src/components/layout/SideBarLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="logo"></div>
+  <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
+    <a-menu-item key="1">
+      <BookOutlined />
+      <span>Book</span>
+    </a-menu-item>
+  </a-menu>
+</template>
+
+<script lang="ts" setup>
+import {
+  BookOutlined,
+} from '@ant-design/icons-vue';
+import { ref } from 'vue';
+const selectedKeys = ref<string[]>(['1']);
+</script>
+
+<style scoped>
+#components-layout-demo-side .logo {
+  height: 32px;
+  margin: 16px;
+  background: rgba(255, 255, 255, 0.3);
+}
+</style>
